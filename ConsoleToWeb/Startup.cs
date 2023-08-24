f5295df8e15584a518c0c7630795c16b093482ed
Suraj <surajpoudelkhatri@gmail.com>
2023-08-25 00:38:51 +0545

    initial commit

---
+++ ConsoleToWeb/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleToWeb
+{
+    public class Startup
+    {
+        public void ConfigurationServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app,IWebHostEnvironment env)
+        {
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                await context.Response.WriteAsync("hello from routing"));
+            });
+        }
+    }
+}
